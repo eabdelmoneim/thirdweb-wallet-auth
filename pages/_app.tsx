04a3271b386496b932be3180b52d3ffaf1131331
Eiman <eabdelmoneim@gmail.com>
2023-06-08 23:27:10 -0500

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import type { AppProps } from "next/app";
+import { ThirdwebProvider, coinbaseWallet, metamaskWallet } from "@thirdweb-dev/react";
+
+// This is the chain your dApp will work on.
+const activeChain = "ethereum";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThirdwebProvider
+      activeChain={activeChain}
+      supportedWallets={[metamaskWallet(), coinbaseWallet()]}
+      authConfig={{
+        domain: "example.com",
+        authUrl: "/api/auth",
+      }}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+}
+
+export default MyApp;
